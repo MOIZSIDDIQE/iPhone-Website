829e7f4ca0e77380932c04afa1cb9549425160d8
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-10-09 23:24:20 +0500

    HTML: Give navbar links. Style: give color, hover color, padding ,transition and center in div

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,13 @@ const Navbar = () => {
     <nav className="w-full flex screen-max-width">
       <img src={appleImg} alt="Apple"
       width={14} height={18} />
+      <div className="flex flex-1 justify-center max-sm:hidden  ">
+        {navLists.map((nav=>(
+          <div key={nav} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
+            {nav}   
+          </div>
+        )))}
+      </div>
       
     </nav>
 </header>
