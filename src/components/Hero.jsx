e069699f9f8141fd82623e9f87fe42cb37e4b60b
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-10-09 23:38:32 +0500

    import gsap and videos which is using in this file

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+import { gsap } from "gsap";
+import { useGSAP } from '@gsap/react';
+import { heroVideo, smallHeroVideo } from '../utils';
+
+
+const Hero = () => {
+
+
+  return (
+    
+  )
+}
+
+export default Hero
