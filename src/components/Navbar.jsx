7ebedc7644697aa76778cd692e848cf7b572991c
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-10-09 23:17:34 +0500

    Add Navbar component with imported assets and navigation list setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { appleImg , bagImg, searchImg } from "../utils";
+import { navLists } from "../constants";
+
+const Navbar = () => {
+  return (
+    
+  )
+}
+export default Navbar
