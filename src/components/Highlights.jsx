24b7b5a85d061cfa17039b011735a5a23cb3ded3
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-10-09 23:48:07 +0500

    feat: create Highlights component with placeholder for future content and GSAP setup

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,15 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import React from 'react'
+import { rightImg, watchImg } from '../utils'
+import VideoCarousel from './VideoCarousel'
+
+const Highlights = () => {
+ 
+
+  return (
+    
+  )
+}
+
+export default Highlights
