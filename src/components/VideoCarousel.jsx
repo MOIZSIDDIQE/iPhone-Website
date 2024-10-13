94e6418b2d035c559a2fd8487d43f081038ea185
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-10-13 23:53:03 +0500

    feat: set up video reference and state management for video playback

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -5,6 +5,19 @@ import { pauseImg, playImg, replayImg } from "../utils";
 import { useGSAP } from "@gsap/react";
 
 const VideoCarousel = () => {
+  const videoRef = useRef([]);
+  const videoSpanRef = useRef([]);
+  const videoDivRef = useRef([]);
+
+  const [Video, setVideo] = useState({
+    isEnd: false,
+    startPlaying: false,
+    videoId: 0,
+    isLastVideo: false,
+    isPlaying: false,
+  });
+
+
 };
 
 export default VideoCarousel;
