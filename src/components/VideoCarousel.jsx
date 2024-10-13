2e657335d9dc2bbae80002a008018b4971242621
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-10-13 23:50:57 +0500

    feat: initialize VideoCarousel component with basic structure and imports

---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,10 @@
+import React, { useRef, useEffect, useState } from "react";
+import { hightlightsSlides } from "../constants";
+import gsap from "gsap";
+import { pauseImg, playImg, replayImg } from "../utils";
+import { useGSAP } from "@gsap/react";
+
+const VideoCarousel = () => {
+};
+
+export default VideoCarousel;
