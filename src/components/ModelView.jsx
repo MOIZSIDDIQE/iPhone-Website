77c409b42ab7671a5ef487fbc0ce1acf2c3e3872
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-11-01 23:53:43 +0500

    import variables

---
+++ src/components/ModelView.jsx
@@ -0,0 +1,6 @@
+import { Html, OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import Lights from './Lights';
+import { Suspense } from 'react';
+import Iphone from './Iphone';
+import { Canvas } from '@react-three/fiber';
+import * as THREE from 'three';
